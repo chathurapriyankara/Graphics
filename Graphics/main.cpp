66fb2128ba78b6f15786f992cf75c763649f8973
chathurapriyankara <priyankarahac@gmail.com>
2020-10-07 18:46:45 +1300

    Initial commit

---
+++ Graphics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Graphics
+//
+//  Created by Chathura Priyankara on 7/10/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
